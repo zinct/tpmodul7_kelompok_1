6c0f368451cf5ee0660ad6329d5922f47844e6eb
febry <febryt22@gmail.com>
2024-04-01 00:21:49 +0700

    menambahkan class DataMahasiswa1302220084

---
+++ tpmodul7_kelompok_1/Program.cs
@@ -0,0 +1,10 @@
+﻿using tpmodul7_kelompok_1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa1302220084 t1 = new DataMahasiswa1302220084();
+        t1.readJSON();
+    }
+}
