732e43513bebedcb264e05fd4adf47e104028ea3
fionadhilla <fiona08dhilla@gmail.com>
2024-04-01 00:49:09 +0700

    menambahkan class Datamahasiswa1302220002

--- tpmodul7_kelompok_1/Program.cs
+++ tpmodul7_kelompok_1/Program.cs
@@ -1,7 +1,48 @@
-﻿internal class Program
+﻿
+using System.Text.Json;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+internal class Program
 {
+
+    
+    class Dataamahasiswa1302220002
+    {
+        public class Mahasiswa1302220002
+        {
+            public Nama nama { get; set; }
+            public int nim { get; set; }
+            public string fakultas { get; set; }
+
+            
+        }
+
+        public class Nama
+        {
+            public string depan { get; set; }
+            public string belakang { get; set; }
+        }
+
+        public void readjson()
+        {
+            string Read_json = File.ReadAllText("../../../tp7_1_1302220002.json");
+            //string json = JsonSerializer.Serialize(new Dataamahasiswa1302220002().);
+            var mahasiswa_1302220002 = JsonSerializer.Deserialize<Mahasiswa1302220002>(Read_json);
+            Console.WriteLine($"{mahasiswa_1302220002.nama.depan} {mahasiswa_1302220002.nama.belakang} dari fakultas {mahasiswa_1302220002.fakultas}");
+        }
+    }
+
+
+    
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var A = new Dataamahasiswa1302220002();
+        A.readjson();
     }
 }
