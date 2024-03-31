d2fd2d3229be1883b91cbce1ad22dc0ab1785901
Indra Mahesa <indramahesa128@gmail.com>
2024-04-01 00:40:33 +0700

    menambahkan class KuliahMahasiswa1302220067

--- tpmodul7_kelompok_1/Program.cs
+++ tpmodul7_kelompok_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Text.Json;
+using static DataMahasiswa1302220067;
 
 internal class Program
 {
@@ -30,5 +31,30 @@ class DataMahasiswa1302220067
         var mahasiswa = JsonSerializer.Deserialize<Mahasiswa>(json, new JsonSerializerOptions { IncludeFields = true });
 
         Console.WriteLine("Nama {0} {1} dengan nim {2}", mahasiswa.nama.depan, mahasiswa.nama.belakang, mahasiswa.nim);
+    }
+}
+
+public class KuliahMahasiswa1302220067
+{
+    public class Matakuliah
+    {
+        public Course[] courses;
+    }
+
+    public class Course
+    {
+        public string code;
+        public string name;
+    }
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("../../../tp7_2_1302220067.json");
+        var matakuliah = JsonSerializer.Deserialize<Matakuliah>(json, new JsonSerializerOptions { IncludeFields = true });
+
+        Console.WriteLine("Daftar mata kuliah yang diambil");
+        for(int i = 0; i < matakuliah.courses.Length; i++)
+        {
+            Console.WriteLine("MK {0} {1}-{2}", i + 1, matakuliah.courses[i].code, matakuliah.courses[i].name);
+        }
     }
 }
